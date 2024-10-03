3e30cb8a4c9047686f32fab22cd8f45ad08e9e42
Ann <socevilaf@gmail.com>
2024-10-03 17:14:24 +0300

    remove-copies

--- ui-kit/Button/ВuttonType.ts
+++
@@ -1,8 +0,0 @@
-export type ButtonTypes = 'button' | 'submit' | 'reset';
-export type ButtonClassName = 'primary' | 'secondary' | 'error';
-
-export type ButtonProps = {
-    children: React.ReactNode
-    type?: ButtonTypes
-    className?: ButtonClassName
-};
